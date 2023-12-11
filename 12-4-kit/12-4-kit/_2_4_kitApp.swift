e2e379acb3f924cd2d2b1d984375267352308bda
Kenny <kennypluchau@gmail.com>
2023-12-11 15:54:29 -0800

    init

---
+++ 12-4-kit/12-4-kit/_2_4_kitApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_4_kitApp.swift
+//  12-4-kit
+//
+//  Created by Kenny Luchau on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct _2_4_kitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
